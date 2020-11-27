f3843a019e773684b94fda8c9157727ce24985f9
DeerKitchen <lushikaibj@163.com>
2020-11-27 16:16:18 +0800

    Test VScode

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(int argc,char** argv) {
     cout << "Enter main function!" << endl;
     cout << "Test git" << endl;
+    cout << "Test VScode git" << endl;
     return 0;
 }
